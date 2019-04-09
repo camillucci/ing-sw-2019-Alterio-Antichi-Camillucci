715b921d32c16bfa13eb5d153cc027775e4b8cb3
fedeAlterio <48481385+fedeAlterio@users.noreply.github.com>
2019-04-09 16:43:44 +0200

    powerUp update

--- src/main/java/it/polimi/ingsw/model/PowerUpCard.java
+++ src/main/java/it/polimi/ingsw/model/PowerUpCard.java
@@ -1,9 +1,28 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.action.SelectionAction;
+import it.polimi.ingsw.model.branch.Branch;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class PowerUpCard {
 
     protected String name;
     protected AmmoColor color;
 
     public abstract void visualize();
+
+    protected List<SelectionAction> powerupModalities = new ArrayList<>();
+
+    public List<Branch> getPowerUpModalities(Player shooter)
+    {
+        buildFireModalities(shooter);
+        ArrayList<Branch> ret = new ArrayList<>();
+        for(SelectionAction a : powerupModalities)
+            ret.add(new Branch(a));
+        return ret;
+    }
+
+    protected abstract void buildFireModalities(Player shooter);
 }
---
+++ src/main/java/it/polimi/ingsw/model/action/PowerUpSelection.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.model.action;
+
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.PowerUpCard;
+import it.polimi.ingsw.model.WeaponCard;
+import it.polimi.ingsw.model.branch.Branch;
+
+import java.util.ArrayList;
+
+public class PowerUpSelection extends SelectionAction
+{
+    public PowerUpSelection(Player ownerPlayer)
+    {
+        super(ownerPlayer);
+        ArrayList<Branch> w = new ArrayList<>();
+        for(PowerUpCard wc : ownerPlayer.getPowerUps())
+        {
+            SelectionAction wi = new SelectionAction(ownerPlayer, wc.getFireModalities(ownerPlayer), wc); //
+            w.add(new Branch(wi));
+        }
+        this.branches = w;
+    }
+}
