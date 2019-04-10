5d20bd410a46cf2941c75f759ea7e1986989b202
fedeAlterio <48481385+fedeAlterio@users.noreply.github.com>
2019-04-10 22:46:44 +0200

    fixing DamageAction

--- src/main/java/it/polimi/ingsw/model/WeaponFactory.java
+++ src/main/java/it/polimi/ingsw/model/WeaponFactory.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.model.action.MarkAction;
-import it.polimi.ingsw.model.action.damageAction;
+import it.polimi.ingsw.model.action.DamageAction;
 import it.polimi.ingsw.model.action.EndBranchAction;
 import it.polimi.ingsw.model.action.SelectionAction;
 import it.polimi.ingsw.model.branch.Branch;
@@ -23,7 +23,7 @@ public class WeaponFactory
     private static void buildWeapons()
     {
         weapons.add(new WeaponCard("LockRifle",
-                new SelectionAction(new Branch(new damageAction(1), new EndBranchAction()), null),
-                new SelectionAction(new Branch(new damageAction(1), new MarkAction(0,1), new EndBranchAction()), null)));
+                new SelectionAction(new Branch(new DamageAction(1), new EndBranchAction()), null),
+                new SelectionAction(new Branch(new DamageAction(1), new MarkAction(0,1), new EndBranchAction()), null)));
     }
 }
--- src/main/java/it/polimi/ingsw/model/action/DamageAction.java
+++ src/main/java/it/polimi/ingsw/model/action/DamageAction.java
@@ -1,36 +1,36 @@
-package it.polimi.ingsw.model.action;
+ package it.polimi.ingsw.model.action;
 
-public class damageAction extends ShootAction
+public class DamageAction extends ShootAction
 {
     private static final int totPlayer = 5;
     private int[] totDamage;
 
-    protected damageAction()
+    protected DamageAction()
     {
         this.shootFuncP = (a,b)->damagePlayer();
     }
 
-    public damageAction(int damageP1)
+    public DamageAction(int damageP1)
     {
         this();
         totDamage = new int[] {damageP1};
     }
-    public damageAction(int damageP1, int damageP2)
+    public DamageAction(int damageP1, int damageP2)
     {
         this();
         totDamage = new int[] {damageP1, damageP2};
     }
-    public damageAction(int damageP1, int damageP2, int damageP3)
+    public DamageAction(int damageP1, int damageP2, int damageP3)
     {
         this();
         totDamage = new int[] {damageP1, damageP2, damageP3};
     }
-    public damageAction(int damageP1, int damageP2, int damageP3, int damageP4)
+    public DamageAction(int damageP1, int damageP2, int damageP3, int damageP4)
     {
         this();
         totDamage = new int[] {damageP1, damageP2, damageP3, damageP4};
     }
-    public damageAction(int damageP1, int damageP2, int damageP3, int damageP4, int damageP5)
+    public DamageAction(int damageP1, int damageP2, int damageP3, int damageP4, int damageP5)
     {
         this();
         totDamage = new int[] {damageP1, damageP2, damageP3, damageP4, damageP5};
