70f299c4e913c7ba84350087c48112a6dde5b9e3
fedeAlterio <48481385+fedeAlterio@users.noreply.github.com>
2019-04-11 13:53:43 +0200

    Manual merge of WeaponFactory

--- src/main/java/it/polimi/ingsw/model/WeaponFactory.java
+++ src/main/java/it/polimi/ingsw/model/WeaponFactory.java
@@ -33,9 +33,30 @@ public class WeaponFactory
         // P1,..,Pn rest the same when andThen function is called()
 
 
-        weapons.add(new WeaponCard("LockRifle",
+        weapons.add(new WeaponCard("LockRifle",  new int[] {1, 0, 0, 2, 0, 0},
                 new SelectionAction(new Branch(new ShootAction(TargetsFilters::visiblePlayers, damageF(2).andThen(markF(1))), new EndBranchAction()), null),
                 new SelectionAction(new Branch(new ShootAction(TargetsFilters::visiblePlayers, damageF(2).andThen(markF(1,1))), new EndBranchAction()), null)));
+/*
+        weapons.add(new WeaponCard("LockRifle", new int[] {1, 0, 0, 2, 0, 0},
+                new SelectionAction(new Branch(new DamageAction(2), new MarkAction(1), new EndBranchAction()), null),
+                new SelectionAction(new Branch(new DamageAction(2), new MarkAction(1,1), new EndBranchAction()), null)));
+
+        weapons.add(new WeaponCard("MachineGun", new int[] {0, 1, 0, 1, 1, 0},
+                new SelectionAction(new Branch(new DamageAction(1), new DamageAction(1), new EndBranchAction()), null),
+                new SelectionAction(new Branch(new DamageAction(1), new EndBranchAction()), null),
+                new SelectionAction(new Branch(new DamageAction(1), new DamageAction(1), new EndBranchAction()), null)));
+
+        weapons.add(new WeaponCard("T.H.O.R", new int[] {0, 1, 0, 1, 1, 0},
+                new SelectionAction(new Branch(new DamageAction(2), new EndBranchAction()), null),
+                new SelectionAction(new Branch(new DamageAction(1), new EndBranchAction()), null),
+                new SelectionAction(new Branch(new DamageAction(2), new EndBranchAction()), null)));
+
+        weapons.add(new WeaponCard("PlasmaGun", new int[] {0, 0, 1, 1, 0, 1},
+                new SelectionAction(new Branch(new DamageAction(2), new EndBranchAction()), null),
+                new SelectionAction(new Branch(new MoveAction(2), new EndBranchAction()), null),
+                new SelectionAction(new Branch(new DamageAction(1), new EndBranchAction()), null)));
+        //TODO need to specify the moveAction is possible both before and after the other effects
+ */
     }
 
     public static BiConsumer<Player, List<Player>> damageF(int ... damage)
