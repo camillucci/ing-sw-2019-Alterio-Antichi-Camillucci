02dd66b2bafdf13e24831b039ffd92ef2b8bc4cd
ClarenceAntichi <48724542+ClarenceAntichi@users.noreply.github.com>
2019-04-24 19:47:12 +0200

    Minor changes in CLI

--- src/main/java/it/polimi/ingsw/view/cli/CLI.java
+++ src/main/java/it/polimi/ingsw/view/cli/CLI.java
@@ -7,11 +7,15 @@ public class CLI implements ViewInterface {
     //TODO private Client client;
     private ParserCLI parser;
     private MassengerCLI massanger;
-
+    private Match match;
 
     public CLI() {
         parser = new ParserCLI();
         massanger = new MassengerCLI();
+    }
+
+    public void startConnection() {
+        //TODO
     }
 
     private void login() {
