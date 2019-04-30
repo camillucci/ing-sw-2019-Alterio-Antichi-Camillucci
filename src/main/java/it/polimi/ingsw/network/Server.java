7825fad8f6afb519d9bf6f75f17a9ea0407da48f
ClarenceAntichi <48724542+ClarenceAntichi@users.noreply.github.com>
2019-04-30 18:18:28 +0200

    Updated Client section

--- src/main/java/it/polimi/ingsw/network/Server.java
+++ src/main/java/it/polimi/ingsw/network/Server.java
@@ -1,19 +1,25 @@
 package it.polimi.ingsw.network;
 
+import it.polimi.ingsw.controller.Room;
 import it.polimi.ingsw.model.Match;
 import it.polimi.ingsw.network.socket.TCPListener;
 
 import java.util.ArrayList;
 
 public class Server {
-    ArrayList<Match> matchList = new ArrayList<Match>();
+    ArrayList<Room> lobby = new ArrayList<Room>();
+    ArrayList<Client> clients = new ArrayList<Client>();
     private final int ip = 10000;
     TCPListener tcpListener = new TCPListener(ip);
 
 
-    public ArrayList<Match> getMatches() {
-        return matchList;
+    public ArrayList<Room> getMatches() {
+        //TODO;
+        return null;
     }
 
+    public Room getJoinableRoom() {
 
+        return null;
+    }
 }
