08269a6b47981df7d5b270354d629afc61b7533b
Camillucci <nicola.camillucci@mail.polimi.it>
2019-05-20 00:03:41 +0200

    Update CLIMessengerTest.java

--- src/test/java/it/polimi/ingsw/view/cli/CLIMessengerTest.java
+++ src/test/java/it/polimi/ingsw/view/cli/CLIMessengerTest.java
@@ -1,5 +1,25 @@
+package it.polimi.ingsw.view.cli;
+
+import it.polimi.ingsw.model.Match;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static it.polimi.ingsw.model.PlayerColor.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CLIMessengerTest {
 
+    @Test
+    void updateView() {
+        Match match = new Match(Arrays.asList("ThisIsAVLongName", "Jimmy", "John"), Arrays.asList(VIOLET, GRAY, YELLOW), 5, 10);
+        match.gameBoard.getPlayers().get(0).setCurrentSquare(match.gameBoard.getSquares().get(0));
+        match.gameBoard.getPlayers().get(0).getCurrentSquare().addPlayer(match.gameBoard.getPlayers().get(0));
+        match.gameBoard.getPlayers().get(1).setCurrentSquare(match.gameBoard.getSquares().get(1));
+        match.gameBoard.getPlayers().get(1).getCurrentSquare().addPlayer(match.gameBoard.getPlayers().get(1));
+        match.gameBoard.getPlayers().get(2).setCurrentSquare(match.gameBoard.getSquares().get(2));
+        match.gameBoard.getPlayers().get(2).getCurrentSquare().addPlayer(match.gameBoard.getPlayers().get(2));
+        CLIMessenger.updateView(match.createSnapshot(0));
+        assertTrue(true);
+    }
 }
