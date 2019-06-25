2ca7dc34959263efd8b795f7fa556fe6b472496b
ClarenceAntichi <48724542+ClarenceAntichi@users.noreply.github.com>
2019-06-25 18:35:09 +0200

    Documentation added

--- src/main/java/it/polimi/ingsw/view/Login.java
+++ src/main/java/it/polimi/ingsw/view/Login.java
@@ -7,19 +7,76 @@ import it.polimi.ingsw.model.snapshots.MatchSnapshot;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Abstract class used to manage all the initial interactions with the user
+ */
 public abstract class Login extends ViewElement
 {
+    /**
+     * Event other classes can subscribe to. This event is used to notify the subscribers when the user chooses
+     * between Socket or RMI connection
+     */
     public final IEvent<Login, Boolean> socketEvent = new Event<>();
+
+    /**
+     * Event other classes can subscribe to. This event is used to notify the subscribers when the user chooses
+     * their username
+     */
     public final IEvent<Login, String> nameEvent = new Event<>();
+
+    /**
+     * Event other classes can subscribe to. This event is used to notify the subscribers when the user chooses
+     * their player color
+     */
     public final IEvent<Login, Integer> colorEvent = new Event<>();
+
+    /**
+     * Event other classes can subscribe to. This event is used to notify the subscribers when the host chooses
+     * the number of skulls of which the game is going to be made of
+     */
     public final IEvent<Login, Integer> gameLengthEvent = new Event<>();
+
+    /**
+     * Event other classes can subscribe to. This event is used to notify the subscribers when the host chooses
+     * the map type the game is going to be played in
+     */
     public final IEvent<Login, Integer> gameMapEvent = new Event<>();
     public final IEvent<Login, Boolean> rmiEvent = new Event<>();
+
+    /**
+     * Event other classes can subscribe to. This event is used to notify the subscribers when all the login tasks
+     * have been finished.
+     */
     public final IEvent<Login, MatchSnapshot> loginCompletedEvent = new Event<>();
+
+    /**
+     * Asks the user about which type of connection they want to use (RMI or Socket)
+     */
     public abstract void askConnection() throws IOException;
+
+    /**
+     * This method is called in case the user's username has already been taken by someone else. It calls the
+     * askName method one more time if the accepted parameter is false.
+     * @param accepted Indicates whether the user's name of choice has been taken or not
+     */
     public abstract void notifyAccepted(boolean accepted) throws IOException;
+
+    /**
+     * Asks which color the user wants to take and sends the answer to the server via invoking the color Event.
+     * @param availableColors
+     */
     public abstract void notifyAvailableColor(List<String> availableColors) throws IOException;
+
+    /**
+     * If the user is the host, then two questions are asked about how they want to set up the game. Then the server
+     * is notified of the user's answer via invoking gameLenghtevent and gameMapEvent
+     * @param isHost Indicates whether the user is the host (first one to enter the room) or not
+     */
     public abstract void notifyHost(boolean isHost) throws IOException;
+
+    /**
+     * Calls the default intro methods. They display the intro script and ask the player for a username
+     */
     public abstract void login() throws IOException;
 
     @Override
--- src/main/java/it/polimi/ingsw/view/cli/LoginCLI.java
+++ src/main/java/it/polimi/ingsw/view/cli/LoginCLI.java
@@ -7,7 +7,15 @@ import it.polimi.ingsw.view.Login;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Class used to manage all the initial interactions with the user, in case they chose a CLI based display model.
+ */
 public class LoginCLI extends Login {
+
+    /**
+     * Calls CLIMessenger's method to display the ask connection question. Makes sure that the answer is acceptable.
+     * If it is, the event is invoked, otherwise the user is asked again.
+     */
     @Override
     public void askConnection() throws IOException {
         CLIMessenger.askConnection();
@@ -17,12 +25,21 @@ public class LoginCLI extends Login {
         ((Event<Login, Boolean>)socketEvent).invoke(this, connection);
     }
 
+    /**
+     * Asks which color the user wants to take and sends the answer to the server via invoking the color Event.
+     * @param availableColors
+     */
     @Override
     public void notifyAvailableColor(List<String> availableColors) throws IOException {
         CLIMessenger.askColor(availableColors);
         ((Event<Login, Integer>)colorEvent).invoke(this, CLIParser.parser.parseIndex(availableColors.size()));
     }
 
+    /**
+     * If the user is the host, then two questions are asked about how they want to set up the game. Then the server
+     * is notified of the user's answer via invoking gameLenghtevent and gameMapEvent
+     * @param isHost Indicates whether the user is the host (first one to enter the room) or not
+     */
     @Override
     public void notifyHost(boolean isHost) throws IOException  {
         if(isHost)
@@ -34,12 +51,20 @@ public class LoginCLI extends Login {
         }
     }
 
+    /**
+     * This method is called in case the user's username has already been taken by someone else. It calls the
+     * askName method one more time if the accepted parameter is false.
+     * @param accepted Indicates whether the user's name of choice has been taken or not
+     */
     @Override
     public void notifyAccepted(boolean accepted) throws IOException {
         if(!accepted)
             askName();
     }
 
+    /**
+     * Calls the default intro methods. They display the intro script and ask the player for a username
+     */
     @Override
     public void login() throws IOException {
         CLIMessenger.intro();
@@ -47,11 +72,19 @@ public class LoginCLI extends Login {
         askName();
     }
 
+    /**
+     * Asks the user to choose a name. Then the name is "filtered" by the parser and the server is notified via
+     * invoking the nameEvent
+     */
     private void askName() throws IOException {
         CLIMessenger.insertName();
         ((Event<Login, String>)nameEvent).invoke(this, CLIParser.parser.parseName());
     }
 
+    /**
+     * Prints a generic message as an output
+     * @param message Displayed message
+     */
     @Override
     public void onNewMessage(String message) {
         CLIMessenger.printMessage(message);
