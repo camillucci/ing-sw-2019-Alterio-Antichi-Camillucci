63b28f822dc5167b3af0e602ebe29d4006598442
ClarenceAntichi <48724542+ClarenceAntichi@users.noreply.github.com>
2019-06-26 17:10:08 +0200

    Documentation added

--- src/main/java/it/polimi/ingsw/model/cards/PowerUpCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/PowerUpCard.java
@@ -7,12 +7,28 @@ import it.polimi.ingsw.model.action.PowerUpAction;
 
 import java.util.function.Supplier;
 
+/**
+ * This class contains all the generic methods and information common between all different types of power up cards.
+ */
 public abstract class PowerUpCard {
 
+    /**
+     * Name that distinguishes the card from the others present in the game
+     */
     public final String name;
+
+    /**
+     * Color of the ammo the card produces by being discarded
+     */
     public final AmmoColor color;
     private Supplier<PowerUpAction> effectBuilder;
 
+    /**
+     * Constructor. It assigns all the input parameters to their global correspondences
+     * @param name Name that distinguishes the card from the others present in the game
+     * @param color Color of the ammo the card produces by being discarded
+     * @param effect //todo
+     */
     public PowerUpCard(String name, AmmoColor color, Supplier<PowerUpAction> effect)
     {
         this.name = name;
@@ -24,14 +40,26 @@ public abstract class PowerUpCard {
         return effectBuilder.get();
     }
 
+    /**
+     * This method calls an add method from the power up set of cards the card needs to added to.
+     * @param powerupSet List of power up cards
+     */
     public abstract void addTo(PowerUpSet powerupSet);
 
+    /**
+     * This method calls an add method from the power up set of cards the card needs to removed from.
+     * @param powerupSet List of power up cards
+     */
     public abstract void removeFrom(PowerUpSet powerupSet);
 
     public String getName() {
         return name.concat(" " + color.getName());
     }
 
+    /**
+     * Calculates which color of ammo the card produces when discarded
+     * @return The color of ammo associated with the card
+     */
     public Ammo colorToAmmo() {
         if(color == AmmoColor.BLUE)
             return new Ammo(1, 0, 0);
