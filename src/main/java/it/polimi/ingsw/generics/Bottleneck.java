33233c7f39d7ea1535e4162b59ca86a832829a6a
ClarenceAntichi <48724542+ClarenceAntichi@users.noreply.github.com>
2019-07-03 20:23:41 +0200

    Documentation added

--- src/main/java/it/polimi/ingsw/generics/Bottleneck.java
+++ src/main/java/it/polimi/ingsw/generics/Bottleneck.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw.generics;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Utility class with the goal of throwing a default exception when a ThrowingAction is invoked and it fails.
+ */
 public class Bottleneck
 {
     public final IEvent<Bottleneck, Exception> exceptionGenerated = new Event<>();
