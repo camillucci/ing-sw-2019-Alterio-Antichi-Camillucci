2290ba21e7120f84f3536cbade84d6851a2d3a36
ClarenceAntichi <48724542+ClarenceAntichi@users.noreply.github.com>
2019-07-05 20:41:33 +0200

    Documentation added

--- src/main/java/it/polimi/ingsw/generics/Visualizable.java
+++ src/main/java/it/polimi/ingsw/generics/Visualizable.java
@@ -2,12 +2,24 @@ package it.polimi.ingsw.generics;
 
 import java.io.Serializable;
 
+/**
+ * Utility class used to convert generic elements into images. It implements serializable because it's purpose is to
+ * support the objects sent from the server to the client (that are eventually going to be displayed to the user).
+ */
 public class Visualizable implements Serializable {
     public final String imgPath;
     public final String icon;
     public final String description;
     public final String name;
 
+    /**
+     * Constructor that assigns every input parameter to their correspondences
+     * @param imgPath Path required to get the image used to represent the object
+     * @param iconPath Path to the icon of the object
+     * @param description Textual description of the object, generically used to add information about the object that
+     *                    can't be included in the name of image.
+     * @param name Name of the represented object
+     */
     public Visualizable(String imgPath, String iconPath, String description, String name)
     {
         this.imgPath = imgPath;
@@ -16,14 +28,32 @@ public class Visualizable implements Serializable {
         this.name = name;
     }
 
+    /**
+     * Alternative constructor that only requires the name, the description and the image path of the object to be
+     * specified.
+     * @param imgPath Path required to get the image used to represent the object
+     * @param description Textual description of the object, generically used to add information about the object that
+     *                    can't be included in the name of image.
+     * @param name Name of the represented object
+     */
     public Visualizable(String imgPath, String description, String name){
         this(imgPath, null, description, name);
     }
 
+    /**
+     * Alternative constructor that only requires the name and the description of the object to be specified.
+     * @param description Textual description of the object, generically used to add information about the object that
+     *                    can't be included in the name of image.
+     * @param name Name of the represented object
+     */
     public Visualizable(String description, String name){
         this(null, description, name);
     }
 
+    /**
+     * Alternative constructor that only requires the name of the object to be specified.
+     * @param name Name of the represented object
+     */
     public Visualizable(String name){
         this("", name);
     }
